9441f475efd79aa231b7cf6c9748ecc52a7e5409
calen_s <calenshirfield@gmail.com>
2023-10-18 11:37:24 +0100

    Some and every method examples.

--- array-functions.js
+++ array-functions.js
@@ -1,28 +1,60 @@
 /*
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
 Map array method
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
 */
 
-let numbers = [1,2,3,4,5,6,7];
-let squareResult = numbers.map(square);
+// let numbers = [1,2,3,4,5,6,7];
+// let squareResult = numbers.map(square);
 
-function square(array) {
-    return x * x;
-}
+// function square(array) {
+//     return x * x;
+// }
 
-/* console.log(numbers);
-console.log(squareResult); */
+// console.log(numbers);
+// console.log(squareResult);
+
+/* ----------------------------------------------------------- */
+
 
 /*
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 Filter array method
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 */
+
+// let numbers = [1,2,3,4,5,6,7];
+
+// function ifEven(array) {
+//     return array.filter( numb => numb % 2 == 0 );
+// }
+
+// const evenResult = ifEven(numbers);
+// console.log(evenResult);
+
+/*----------------------------------------------------------- */
+
+
+
+/*
+Some and Every
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+*/
+
+
+let numbers = [1,2,3,4,5,6,7];
+
+function isEven(x) {
+    return x % 2 == 0;
+}
+
 
 function ifEven(array) {
     return array.filter( numb => numb % 2 == 0 );
 }
 
 const evenResult = ifEven(numbers);
-console.log(evenResult);
 
+
+console.log('Is some number even ?: ' + evenResult.some(isEven));
+console.log('Is every number even ?: ' + evenResult.every(isEven));
 
