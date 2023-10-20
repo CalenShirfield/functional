8e6266169960f5edf12b382c85e2124444718c7d
calen_s <calenshirfield@gmail.com>
2023-10-20 10:02:17 +0100

    Update to spread and reduce examples.

--- spread-operator.js
+++ spread-operator.js
@@ -4,6 +4,7 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 Spread & reduce
 */
 
+/* Using spread operator to *indirectly* update values of an object  */
 
 const animal = {
     name: 'puppy',
@@ -11,27 +12,54 @@ const animal = {
     breed: 'terrier'
 };
 
-/* missing ellipses... */
-// const updateAnimal = { animal, name: 'paws'};
+/* see what happens with missing ellipses... */
+/*  const updateAnimal = { animal, name: 'paws'}; */
+/* console.log(updateAnimal); */
 
-
-/*
-const updateAnimal = { ...animal, name: 'paws'};
-
-console.log(updateAnimal);
- */
+/* Now with ellipses... */
+/* const updateAnimal = { ...animal, name: 'paws'}; */
+/* console.log(updateAnimal); */
 
 // ----------------------------------------------------------------------------------------
 
 
-const s = [1,2];
+/* Allow a flexible nuber of arguments  */
 
 
-
-function add(x,...numbers) {
+function add(...numbers) {
     return numbers.reduce( (sum, x) => sum + x);
 }
 
+/*
+console.log(add(1, 2));
+console.log(add(1, 2, 3));
+console.log(add(1, 2, 3, 4));
+*/
+
+// ----------------------------------------------------------------------------------------
+
+/* Spread with arrays */
+
+const numbers = [1,2,3,4,5,6,7,8,9,];
+
+const updateNumbers = [0, ...numbers, 10];
+
+/* console.log(updateNumbers); */
+
+/* Pass array elements as individual arguments to functions */
 
 
-console.log(add(1,s));
+/*
+Example of passing numbers array as argument to function
+Notice array is paassed as a single argument
+*/
+
+/* console.log(numbers); */
+
+/* Now array passed with spread */
+
+console.log(add(...numbers)); // -> 45
+
+
+
+
